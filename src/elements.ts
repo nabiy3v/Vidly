d031257b164f1378be08a27c25cf536c9016b96d
Abduvoris Nabiyev <nabduvoris5228@gmail.com>
2025-03-19 00:34:52 +0500

    Added Sort Genres and Delete

--- src/elements.ts
+++ src/elements.ts
@@ -2,3 +2,7 @@ export const sortGenres: NodeListOf<HTMLButtonElement> = document.querySelectorA
 export const genres: NodeListOf<HTMLParagraphElement> = document.querySelectorAll('.genre')!;
 export const hearts: NodeListOf<HTMLSpanElement> = document.querySelectorAll('.heart')!;
 export const deleteBtns: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.delete')!;
+export const pageBtns: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.page-btn')!;
+export const movies: NodeListOf<HTMLElement> = document.querySelectorAll('.movie')!;
+export const movies2: NodeListOf<HTMLElement> = document.querySelectorAll('.movie2')!;
+export const movies3: NodeListOf<HTMLElement> = document.querySelectorAll('.movie3')!;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { sortGenres, genres, hearts, deleteBtns } from "./elements";
+import { sortGenres, genres, hearts, deleteBtns, pageBtns, movies, movies2, movies3 } from "./elements";
 
 function handleClickGenre(e: MouseEvent) {
   let target = e.target as HTMLButtonElement;
@@ -37,6 +37,27 @@ function handleClickDeleteBtn(e: MouseEvent) {
   target.parentElement?.parentElement?.remove();
 }
 
+function handleClickPageBtn(e: MouseEvent) {
+  let target = e.target as HTMLButtonElement;
+  let value = Number(target.dataset.page);
+
+  movies.forEach(movie => movie.classList.remove('hidden'));
+  movies2.forEach(movie2 => movie2.classList.remove('hidden'));
+  movies3.forEach(movie3 => movie3.classList.remove('hidden'));
+
+  if (value === 1) {
+    movies2.forEach(movie2 => movie2.classList.add('hidden'));
+    movies3.forEach(movie3 => movie3.classList.add('hidden'));
+  } else if (value === 2) {
+    movies.forEach(movie => movie.classList.add('hidden'));
+    movies3.forEach(movie3 => movie3.classList.add('hidden'));
+  } else if (value === 3) {
+    movies.forEach(movie => movie.classList.add('hidden'));
+    movies2.forEach(movie2 => movie2.classList.add('hidden'));
+  }
+}
+
+pageBtns.forEach(pageBtn => pageBtn.addEventListener('click', handleClickPageBtn));
 sortGenres.forEach(genre => genre.addEventListener('click', handleClickGenre));
 hearts.forEach(heart => heart.addEventListener('click', handleClickHeart));
 deleteBtns.forEach(deleteBtn => deleteBtn.addEventListener('click', handleClickDeleteBtn));

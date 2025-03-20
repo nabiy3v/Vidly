681b5126469db8a26a9203e50970b77740eccb35
Abduvoris Nabiyev <nabduvoris5228@gmail.com>
2025-03-20 23:53:04 +0500

    Merged

--- src/elements.ts
+++ src/elements.ts
@@ -11,3 +11,14 @@ export const registerInputPassword=document.querySelector("#registerInputPasswor
 export const registerInputName=document.querySelector("#registerInputName")as HTMLInputElement;
 export const registerBtn=document.querySelector("#registerBtn")!;
 
+export const sortGenres: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.sortGenres')!;
+export const genres: NodeListOf<HTMLParagraphElement> = document.querySelectorAll('.genre')!;
+export const hearts: NodeListOf<HTMLSpanElement> = document.querySelectorAll('.heart')!;
+export const deleteBtns: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.delete')!;
+export const pageBtns: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.page-btn')!;
+export const movies: NodeListOf<HTMLElement> = document.querySelectorAll('.movie')!;
+export const movies2: NodeListOf<HTMLElement> = document.querySelectorAll('.movie2')!;
+export let movies3: NodeListOf<HTMLElement> = document.querySelectorAll('.movie3')!;
+export const movieBtn: HTMLButtonElement = document.querySelector('.movieBtn')!;
+export const tableElm: HTMLButtonElement = document.querySelector('.tableElm')!;
+export const searchInput: HTMLButtonElement = document.querySelector('.searchInp')!;
